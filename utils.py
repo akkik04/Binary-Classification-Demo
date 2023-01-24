70d29104e072e22d9084ee161abb6dd6b5beb1bd
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-01-24 11:47:14 -0500

    Defined cost func, sigmoid func, init. data visualization

--- utils.py
+++ utils.py
@@ -7,3 +7,12 @@ def load_data(filename):
     X = data[:,:2]
     y = data[:,2]
     return X, y
+
+# function used to plot data.
+def plot_data(X, y, positive_class_label="y=1", negative_class_label="y=0"):
+    pos = y == 1
+    neg = y == 0
+    
+    # plot examples
+    plt.plot(X[pos, 0], X[pos, 1], 'kx', label=positive_class_label)
+    plt.plot(X[neg, 0], X[neg, 1], 'rx', label=negative_class_label)
